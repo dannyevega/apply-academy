f4868051a8252bdf61c28d611d9630dc9ec546d9
Danny Vega <dannyevega@gmail.com>
2017-06-15 13:14:53 -0700

    added solution for move zeros

--- algorithms.js
+++ algorithms.js
@@ -764,6 +764,49 @@ output = [1, 2, 1, 0]
 
 
 
+/* Given an array, move all zeros to the end. The order of non-zero elements does not matter */
+function moveZeros(array){
+	var zeroCounter = array.length - 1;
+	for(var i = 0; i < array.length && i < zeroCounter; i++){
+		while(array[zeroCounter] === 0){
+			zeroCounter--;
+		}
+		if(array[i] === 0){
+			swap(i, zeroCounter, array);
+		}
+	}
+
+	function swap(a, b, arr){
+		var temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+	return array;
+}
+input = [1,0,0,4,5,0,9]
+output = [1,4,5,9,0,0,0]
+
+function swap(a, b, arr) {
+    console.log(`  swapping ${a} for ${b} in ${arr}`);
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+    console.log(`    > ${arr}`);
+}
+function moveZeros(array) {
+    var zeroCounter = array.length - 1;
+    for (var i = 0; i < array.length && i < zeroCounter; i++) {
+        while (array[zeroCounter] === 0) {
+          zeroCounter--;
+          console.log("zeroCounter: " + zeroCounter);
+        }
+        if (array[i] === 0) {
+          swap(i, zeroCounter, array);
+        }
+        console.log("incrementing i to " + i);
+    }
+    return array;
+}
 
 
 
