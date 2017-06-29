c33e6b06554b6361b1db2b35a6d7c6626fe09079
Danny Vega <dannyevega@gmail.com>
2017-06-29 10:45:37 -0700

    left instructions for gas station problem

--- algorithms.js
+++ algorithms.js
@@ -1036,8 +1036,13 @@ function tString(str){
 
 
 
-gas = [3,5,15]
-cost = [5,6,4]
+/*
+There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+
+Return the minimum starting gas station’s index if you can travel around the circuit once, otherwise return -1
+*/
 function gasStation(gas, cost){
 	for(let i = 0; i < gas.length; i++){
 		let result = canComplete(gas, cost, i);
@@ -1059,6 +1064,8 @@ function gasStation(gas, cost){
 	}
 	return -1;
 }
+gas = [3,5,15]
+cost = [5,6,4]
 
 
 
