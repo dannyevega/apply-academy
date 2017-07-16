4a1daf42f480c9d2fdca6b0a8af6e705de59eaec
Danny Vega <dannyevega@gmail.com>
2017-07-15 19:04:28 -0700

    added method for adding node in bst

--- bst.js
+++ bst.js
@@ -3,6 +3,36 @@ function Node(data){
 	this.left = null;
 	this.right = null;
 }
+
 function BST(){
 	this.root = null;
+}
+
+BST.prototype.add = function(data){
+	let current = this.root;
+	if(!current){
+		this.root = new Node(data);
+		return;
+	} else {
+		let search = function(current){
+			if(data < current.data){
+				if(current.left === null){
+					current.left = new Node(data);
+					return;
+				} else {
+					return search(current.left);
+				}
+			} else if (data > current.data){
+				if(current.right === null){
+					current.right = new Node(data);
+					return;
+				} else {
+					return search(current.right);
+				}
+			} else {
+				return null;
+			}
+		}
+		return search(current);
+	}
 }
