a1329c7c6189bab95cf4cc992e0c07128273c0e0
Danny Vega <dannyevega@gmail.com>
2017-07-15 19:05:15 -0700

    added methods for finding the min and max in bst

--- bst.js
+++ bst.js
@@ -36,3 +36,30 @@ BST.prototype.add = function(data){
 		return search(current);
 	}
 }
+
+let bst = new BST();
+bst.add(5)
+bst.add(3)
+bst.add(7)
+bst.add(2)
+bst.add(4)
+bst.add(6)
+bst.add(8)
+bst.add(1)
+bst.add(9)
+
+BST.prototype.findMin = function(){
+	let current = this.root;
+	while(current.left){
+		current = current.left;
+	}
+	return current.data;
+}
+
+BST.prototype.findMax = function(){
+	let current = this.root;
+	while(current.right){
+		current = current.right;
+	}
+	return current.data;
+}
