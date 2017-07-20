bf748ad74b4c5eb0b59efccca02513fbd49e827e
Danny Vega <dannyevega@gmail.com>
2017-07-20 14:35:02 -0700

    aadded solution for finding area of polyogn

--- algorithms.js
+++ algorithms.js
@@ -1294,3 +1294,16 @@ function adjacentElementsProduct(inputArray) {
     return highest;
 }
 
+
+
+
+//Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+function shapeArea(n) {
+    let area = 1;
+    while(n > 1){
+        area += ((n - 1) * 4);
+        n--;
+    }
+    return area;
+}
+
