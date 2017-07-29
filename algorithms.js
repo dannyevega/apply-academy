cd6bee4247d2d274550fd1a9ce4afc4bc35c1300
Danny Vega <d_vega@apple.com>
2017-07-29 14:09:46 -0700

    removed space

--- algorithms.js
+++ algorithms.js
@@ -1277,10 +1277,8 @@ function telephone(str){
 
 
 /* Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
-
 For inputArray = [3, 6, -2, -5, 7, 3], the output should be
 adjacentElementsProduct(inputArray) = 21.
-
 7 and 3 produce the largest product. */
 function adjacentElementsProduct(inputArray) {
     let highest = inputArray[0] * inputArray[1], temp;
