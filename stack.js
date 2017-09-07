183cca3bfc6feb4a6e4b9189328d46a07c611e48
Danny Vega <dannyevega@gmail.com>
2017-09-07 11:33:34 -0700

    added stack file

---
+++ stack.js
@@ -0,0 +1,9 @@
+Stack time complexity
+╔═══════════╦═════════╦════════════╗
+║ Algorithm ║ Average ║ Worst Case ║
+╠═══════════╬═════════╬════════════╣
+║ Space     ║ O(n)    ║ O(n)       ║
+║ Search    ║ O(n)    ║ O(n)       ║
+║ Insert    ║ O(1)    ║ O(1)       ║
+║ Delete    ║ O(1)    ║ O(1)       ║
+╚═══════════╩═════════╩════════════╝
