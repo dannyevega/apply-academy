4523f6ff84f716b9fea6a6178a01ad9e7f96fba6
Danny Vega <dannyevega@gmail.com>
2017-09-09 17:38:28 -0700

    added solution for finding prime number

--- algorithms.js
+++ algorithms.js
@@ -1123,6 +1123,14 @@ function findPrimeFactors(num){
 	return result;
 }
 
+function isPrime(num){
+	for(var i = 2; i * i <= num; i++){
+		if(num % i === 0){
+			return false;
+		}
+	}
+	return true;
+}
 
 
 
